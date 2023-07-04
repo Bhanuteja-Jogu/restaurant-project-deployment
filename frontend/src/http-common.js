5504f46c690bc70e497734f7829a907fc8306b41
Bhanuteja-Jogu <bhanutejajogu08@gmail.com>
2023-07-04 19:52:05 +0530

    updated url

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-    baseURL:"https://bhanuteja-restaurant-project-api.onrender.com",
+    baseURL:"https://bhanuteja-restaurant-project-api.onrender.com/restaurants",
     headers:{
         "Content-Type": "application/json"
     }
